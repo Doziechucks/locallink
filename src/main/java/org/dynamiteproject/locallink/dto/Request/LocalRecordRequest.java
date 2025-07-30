25134ea0080c7b69331549ef9d0bfd5d1e380596
Doziechucks <chiedoziechukwukam@gmail.com>
2025-07-30 13:48:07 +0100

    dockerized project initial commit

--- src/main/java/org/dynamiteproject/locallink/dto/Request/LocalRecordRequest.java
+++ src/main/java/org/dynamiteproject/locallink/dto/Request/LocalRecordRequest.java
@@ -2,6 +2,7 @@ package org.dynamiteproject.locallink.dto.Request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.List;
--- src/test/java/org/dynamiteproject/locallink/service/AuthenticationServiceImplTest.java
+++ src/test/java/org/dynamiteproject/locallink/service/AuthenticationServiceImplTest.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.dynamiteproject.locallink.LocalLinkApplication;
 import org.dynamiteproject.locallink.config.TestSecurityConfig;
 import org.dynamiteproject.locallink.data.model.Local;
+import org.dynamiteproject.locallink.dto.Request.LocalRecordRequest;
 import org.dynamiteproject.locallink.dto.Request.LocalRegistrationRequest;
+import org.dynamiteproject.locallink.dto.Response.LocalRecordResponse;
 import org.dynamiteproject.locallink.dto.Response.LocalRegistrationResponse;
 import org.junit.jupiter.api.*;
 import org.slf4j.LoggerFactory;
@@ -22,6 +24,10 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
 @Slf4j
 @SpringBootTest
 @Testcontainers
@@ -33,7 +39,10 @@ public class AuthenticationServiceImplTest {
     static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
 
     @Autowired
-    private AuthenticationServiceImpl service;
+    private AuthenticationServiceImpl authService;
+
+    @Autowired
+    private UserServiceImpl userService;
 
     @Autowired
     private MongoTemplate mongoTemplate;
@@ -64,7 +73,7 @@ public class AuthenticationServiceImplTest {
 
         );
 
-        LocalRegistrationResponse response = service.registerLocal(registrationRequest);
+        LocalRegistrationResponse response = authService.registerLocal(registrationRequest);
 
         // Only assert the fields you expect in the response
         Assertions.assertNotNull(response);
@@ -79,6 +88,23 @@ public class AuthenticationServiceImplTest {
         Assertions.assertEquals("Money", saved.getLastname());
         Assertions.assertEquals("dozie@gmail.com", saved.getEmail());
         // Add other field assertions as needed
+    }
+
+    @Test
+    void TestCanCreateRecord(){
+        LocalRecordRequest recordRequest = new LocalRecordRequest(
+                "More money",
+                "I need 1billion dollars",
+                "oka101",
+                "Dozie!23",
+                List.of("manowar"),  // Correct way to create a List with one element
+                new BigDecimal("105000000")  // Note: BigDecimal should be created from a String for precise values
+        );
+
+        LocalRecordResponse response = userService.createRecord(recordRequest);
+        Assertions.assertNotNull(response);
+        log.info("Response: {}", response);
+        Assertions.assertEquals("More money", response.getTitle());
     }
 }
     // Provide necessary beans for the test context
