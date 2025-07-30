37925b8c333dcfc0da60c7a1a27019d8a2d6b054
Doziechucks <chiedoziechukwukam@gmail.com>
2025-07-30 19:15:48 +0100

    calling mongo

--- src/main/java/org/dynamiteproject/locallink/LocalLinkApplication.java
+++ src/main/java/org/dynamiteproject/locallink/LocalLinkApplication.java
@@ -3,12 +3,18 @@ package org.dynamiteproject.locallink;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.Security;
+
 @SpringBootApplication
 public class LocalLinkApplication {
-
+    static {
+        // Force TLS 1.2 for MongoDB connection
+        System.setProperty("jdk.tls.client.protocols", "TLSv1.2");
+        System.setProperty("https.protocols", "TLSv1.2");
+        Security.setProperty("jdk.tls.disabledAlgorithms", "");
+    }
     public static void main(String[] args) {
         SpringApplication.run(LocalLinkApplication.class, args);
-        System.out.println("MONGODB_URI: " + System.getenv("MONGODB_URI"));
     }
 
 }
